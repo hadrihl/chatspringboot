21f7ac80fc8085d40280335ca592b09299fe7088
hadrihl <hadrihl@outlook.com>
2024-03-07 22:50:43 +0800

    Initial commit.

---
+++ src/main/java/com/example/chatspringboot/ChatspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatspringboot/ChatspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
